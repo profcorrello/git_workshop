8e76a9c74144397f256cfdfee7968d6c08a0150d
Clayton Corrello <clayton.corrello@muskegoncc.edu>
2025-03-12 18:07:56 -0400

    Iteration ONE

---
+++ My Special Calculator/src/mcc/examples/calc/Calculator.java
@@ -0,0 +1,36 @@
+package mcc.examples.calc;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter first number: ");
+			double num1 = scanner.nextDouble();
+			System.out.println("Enter an operator (+, -, *, /): ");
+			char operator = scanner.next().charAt(0);
+			System.out.println("Enter second number: ");
+			double num2 = scanner.nextDouble();
+			double result = 0;
+			switch (operator) {
+			case '+':
+				result = num1 + num2;
+				break;
+			case '-':
+				result = num1 - num2;
+				break;
+			case '*':
+				result = num1 * num2;
+				break;
+			case '/':
+				result = num2 != 0 ? num1 / num2 : 0;
+				break;
+			default:
+				System.out.println("Invalid operator");
+				continue;
+			}
+			System.out.println("Result: " + result);
+		}
+	}
+}
