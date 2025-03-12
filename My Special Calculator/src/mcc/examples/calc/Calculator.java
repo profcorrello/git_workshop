6bb2d509a95318e1e2ab76a96442aa52a39cb12b
Clayton Corrello <clayton.corrello@muskegoncc.edu>
2025-03-12 18:12:40 -0400

    Iteration TWO

--- My Special Calculator/src/mcc/examples/calc/Calculator.java
+++ My Special Calculator/src/mcc/examples/calc/Calculator.java
@@ -3,34 +3,36 @@ package mcc.examples.calc;
 import java.util.Scanner;
 
 public class Calculator {
+	static double lastResult = 0; // Memory feature to store the latest result
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
-			System.out.println("Enter first number: ");
-			double num1 = scanner.nextDouble();
-			System.out.println("Enter an operator (+, -, *, /): ");
+			System.out.println("Enter first number (or type 'M' to use last result): ");
+			String input = scanner.next();
+			double num1 = input.equalsIgnoreCase("M") ? lastResult : Double.parseDouble(input);
+			System.out.print("Enter operator (+, -, *, /): ");
 			char operator = scanner.next().charAt(0);
-			System.out.println("Enter second number: ");
+			System.out.print("Enter second number: ");
 			double num2 = scanner.nextDouble();
-			double result = 0;
 			switch (operator) {
 			case '+':
-				result = num1 + num2;
+				lastResult = num1 + num2;
 				break;
 			case '-':
-				result = num1 - num2;
+				lastResult = num1 - num2;
 				break;
 			case '*':
-				result = num1 * num2;
+				lastResult = num1 * num2;
 				break;
 			case '/':
-				result = num2 != 0 ? num1 / num2 : 0;
+				lastResult = num2 != 0 ? num1 / num2 : 0;
 				break;
 			default:
-				System.out.println("Invalid operator");
+				System.out.println("Invalid operator.");
 				continue;
 			}
-			System.out.println("Result: " + result);
+			System.out.println("Result: " + lastResult);
 		}
 	}
 }
