eb4a711b1ce11ae0ebd8ac2ab52851f6910a6985
Clayton Corrello <clayton.corrello@muskegoncc.edu>
2025-04-06 10:08:25 -0400

    Revert "Updated test cases to resolve failures"
    This reverts commit ffb58e910774692d821b590fdb313a2cea040553.

--- calculator-app/src/test/java/CalculatorTest.java
+++ calculator-app/src/test/java/CalculatorTest.java
@@ -20,18 +20,15 @@ class CalculatorTest {
         int a = Integer.MAX_VALUE;
         int b = 1;
         int result = calculator.add(a, b);
-        assertEquals(result, Integer.MIN_VALUE);
-//        assertTrue(result == Integer.MIN_VALUE);
+        assertTrue(result < 0);
     }
 
     @Test
     void testIntegerUnderflow() {
         int a = Integer.MIN_VALUE;
-        int b = 1;
+        int b = -1;
         int result = calculator.subtract(a, b);
-        System.out.println("result: " + result + " Max Value: " + Integer.MAX_VALUE);	
-        assertEquals(result, Integer.MAX_VALUE);
-//        assertTrue(result == Integer.MAX_VALUE);
+        assertTrue(result > 0);
     }
 
     @ParameterizedTest
